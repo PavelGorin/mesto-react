f000cd844b0d628c39fcf7ecc62a5c9dbaf4718e
Pavel Gorin <mr.gorinpavel@yandex.ru>
2021-03-10 03:01:28 +0300

    hashrouter

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter >
+    <HashRouter basename={/mesto-react}>
       <App />
     </HashRouter>
   </React.StrictMode>,
